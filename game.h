d930af4f8899cd33d9b072b7ec0738a4d9a11a26
gbaud05 <150195112+gbaud05@users.noreply.github.com>
2025-12-06 17:16:25 +0100

    modification

--- game.h
+++ game.h
@@ -2,5 +2,5 @@
 
 #include "board.h"
 
-// allow_resume: 0 = always start new (ignore save), 1 = allow resume from save
+//allow_resume:0=commence nouvelle partie,1=autorise reprise depuis la sauvegarde
 void game_run(int allow_resume);
